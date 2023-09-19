2b99dd8d43df367a7c8fd11cad35b31321b763e6
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-09-19 09:48:06 +0200

    Move parameter inputs to a new file

--- src/simulation.jl
+++ src/simulation.jl
@@ -52,80 +52,6 @@ function SimulationSetup(T, ncycles, outdir="", _printevery=1000)
 end
 
 
-abstract type TemperatureFunction <: Function end
-
-struct TRamp <: TemperatureFunction
-    start::typeof(1.0u"K")
-    Δ::typeof(1.0u"K")
-
-    function TRamp(start::typeof(1.0u"K"), Δ::typeof(1.0u"K"), ::Nothing)
-        new(start, Δ)
-    end
-end
-function TRamp(start, stop)
-    TRamp(convert(typeof(1.0u"K"), start), convert(typeof(1.0u"K"), stop-start), nothing)
-end
-function (tf::TRamp)(k, n)
-    tf.start + tf.Δ*(k-1)/(n-1)
-end
-
-struct TPoly <: TemperatureFunction
-    ramp::TRamp
-    γ::Float64
-end
-function (tf::TPoly)(k, n)
-    tf.ramp.start + tf.ramp.Δ*((k-1)/(n-1))^tf.γ
-end
-
-struct TSteps <: TemperatureFunction
-    ramp::TRamp
-    nstepsp1::Int
-end
-function (tf::TSteps)(k, n)
-    tf.ramp.start + tf.Δ*floor(tf.nstepsp1*(k-1)/(n-1))/tf.nstepsp1
-end
-
-struct TParts{N,T} <: TemperatureFunction
-    fractions::NTuple{N,Float64} # sorted between 0 and 1, the last element is always 1
-    parts::T
-end
-function (tf::TParts{N})(k, n) where N
-    ρ = (k-1)/(n-1)
-    for i in 1:N
-        frac = tf.fractions[i]
-        if frac ≥ ρ
-            num_start = i==1 ? -1 : floor(Int, n*tf.fractions[i-1])-1
-            num_stop = floor(Int, n*frac)
-            num_current = floor(Int, n*ρ)
-            return tf.parts[i](num_current - num_start, num_stop - num_start)
-        end
-    end
-    @assert false
-    0.0u"K"
-end
-
-function TAnneal(bottom::typeof(1.0u"K"), top::typeof(1.0u"K"), wait::Float64)
-    TParts(((1.0 - wait)/2, (1.0 + wait)/2, 1.0), (
-        TRamp(bottom, top),
-        Returns(top),
-        TRamp(top, bottom)
-    ))
-end
-function TAnneal(sequence, wait::Number)
-    N = length(sequence)
-    TParts(ntuple(i -> begin
-                        j, o = fldmod(i, 2)
-                        (j + o*(1-wait))/(N-1)
-                       end, 2*(N-1)),
-           ntuple(i -> begin
-                        j, o = fldmod1(i, 2)
-                        seqjp1 = sequence[j+1]
-                        o == 1 ? TRamp(sequence[j], seqjp1) : Returns(seqjp1)
-                       end, 2*(N-1))
-    )
-end
-
-
 
 """
     run_montecarlo!(mc::MonteCarloSetup, T, nsteps::Int)
@@ -229,19 +155,4 @@ function run_montecarlo!(mc::MonteCarloSetup, simu::SimulationSetup)
     end
     wait(output_task[])
     reports
-end
-
-
-abstract type RecordFunction <: Function end
-mutable struct RMinimumEnergy <: RecordFunction
-    mine::BaselineEnergyReport
-    minpos::OutputSimulationStep
-    RMinimumEnergy() = new(BaselineEnergyReport(Inf*u"K", Inf*u"K", Inf*u"K", Inf*u"K", Inf*u"K"))
-end
-function (record::RMinimumEnergy)(o::OutputSimulationStep, e::BaselineEnergyReport, _, _, _)
-    if Float64(e) < Float64(record.mine)
-        record.mine = e
-        record.minpos = o
-    end
-    nothing
 end
