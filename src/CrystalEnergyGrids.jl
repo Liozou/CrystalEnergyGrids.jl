78de78858c5595684776da4378a4caa1bf47a1ef
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-11-21 12:11:28 +0100

    Reproducible single file

--- src/CrystalEnergyGrids.jl
+++ src/CrystalEnergyGrids.jl
@@ -1,14 +1,56 @@
 module CrystalEnergyGrids
 
-using Random: rand, default_rng, TaskLocalRNG
-using StaticArrays
 using Base.Threads
 
+struct LoadBalancer{T}
+    channel::Channel{T}
+    tasks::Vector{Task}
+    event::Event
+end
 
-include("utils.jl")
-include("montecarlo.jl")
-include("simulation.jl")
-include("parameterinputs.jl")
+function LoadBalancer{T}(f, n::Integer=nthreads()-1) where T
+    event::Event = Event(true)
+    channel::Channel{T} = Channel{T}(Inf)
+    tasks = [errormonitor(@spawn while true
+        x = take!($channel)
+        $f(x)
+        notify($event)
+    end) for _ in 1:n]
+    LoadBalancer{T}(channel, tasks, event)
+end
 
+Base.put!(lb::LoadBalancer, x) = put!(lb.channel, x)
+
+function Base.wait(lb::LoadBalancer)
+    while !isempty(lb.channel)
+        wait(lb.event)
+    end
+end
+
+struct Setup
+    positions::Vector{Float64}
+end
+
+function run(setup::Setup, lb::LoadBalancer)
+    for _ in 1:10
+        pos = copy(setup.positions)
+        put!(lb, pos)
+        yield()
+    end
+    wait(lb)
+end
+
+
+function ignore_args(_)
+    Float64[rand()]
+end
+
+function main()
+    for _ in 1:100000
+        setup = Setup(rand(300))
+        lb = LoadBalancer{Vector{Float64}}(ignore_args, 9)
+        run(setup, lb)
+    end
+end
 
 end
--- src/montecarlo.jl
+++
@@ -1,8 +0,0 @@
-struct MonteCarloSetup{Trng}
-    positions::Vector{SVector{3,Float64}}
-    rng::Trng
-end
-
-function setup_montecarlo()
-    MonteCarloSetup([rand(SVector{3,Float64}) for _ in 1:80], default_rng())
-end
