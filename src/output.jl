04a5ba98e80c846b865eb77b0478dfeca5681c6f
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-11-26 20:38:59 +0100

    Output reduction 1

--- src/output.jl
+++ src/output.jl
@@ -62,69 +62,22 @@ function output_restart(path, o::SimulationStep, (a, b, c), (α, β, γ), molnam
 number-of-unit-cells: 1 1 1""")
         for s in ("unit-cell-vector-", "cell-vector-")
             for (i, x) in enumerate(('a', 'b', 'c'))
-                @printf io "%s%c:%19.12f%19.12f%19.12f\n" s x NoUnits(o.mat[1,i]/u"Å") NoUnits(o.mat[2,i]/u"Å") NoUnits(o.mat[3,i]/u"Å")
+                @printf io "%19.12f%19.12f%19.12f\n" NoUnits(o.mat[1,i]/u"Å") NoUnits(o.mat[2,i]/u"Å") NoUnits(o.mat[3,i]/u"Å")
             end
-            println(io)
         end
-        @printf io "cell-lengths:%19.12f%19.12f%19.12f\n" NoUnits(a/u"Å") NoUnits(b/u"Å") NoUnits(c/u"Å")
-        @printf io "cell-angles:%19.12f%19.12f%19.12f\n\n" α β γ
-        println(io, """
-
-Maximum changes for MC-moves:
-========================================================================
-Maximum-volume-change: 0.000500
-Maximum-Gibbs-volume-change: 0.000500
-Maximum-box-shape-change: 0.100000 0.100000 0.100000, 0.100000 0.100000 0.100000, 0.100000 0.100000 0.100000
-
-
-Acceptance targets for MC-moves:
-========================================================================
-Target-volume-change: 0.500000
-Target-box-shape-change: 0.500000
-Target-Gibbs-volume-change: 0.500000
-
-
-Components: """, length(o.ffidx), " (Adsorbates ", sum(length, positions), """, Cations 0)
-========================================================================""")
-        for (i, name) in enumerate(molnames)
-            im1 = i-1
-            println(io, "Component ", im1, " (", name, ')')
-            println(io, "	Fractional-molecule-id component ", im1, ": -1")
-            print(io, "	Lambda-factors component ", im1, ": ")
-            join(io, " 0.000000" for _ in o.ffidx[i])
-            println(io)
-            println(io, "	Number-of-biasing-factors component ", im1, ": 21")
-            println(io, "	Biasing-factors component ", im1, ":  0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000 0.000000")
-            println(io, "	Maximum-CF-Lambda-change component ", im1, ": 0.500000")
-            println(io, "	Maximum-CBCF-Lambda-change component ", im1, ": 0.500000\n")
-            println(io, "	Maximum-translation-change component ", im1, ": 0.246897,0.246897,0.246897")
-            println(io, "	Maximum-translation-in-plane-change component ", im1, ": 0.000000,0.000000,0.000000")
-            println(io, "	Maximum-rotation-change component ", im1, ": 0.123456 0.123456 0.123456")
-        end
-        println(io, "\nReactions: 0\n")
         for (i, name) in enumerate(molnames)
             posi = positions[i]
             num = length(posi)
             ffidxi = o.ffidx[i]
             m = length(ffidxi)
-            println(io, "Component: ", i-1, "     Adsorbate    ", num, " molecules of ", name)
             println(io, "------------------------------------------------------------------------")
             for (j, poss) in enumerate(posi), (k, opos) in enumerate(poss)
                 abc = invmat * opos
                 pos = NoUnits.(o.mat*(abc .- floor.(abc))./u"Å")
                 @printf io "Adsorbate-atom-position: %d %d%19.12f%19.12f%19.12f\n" (j-1) (k-1) pos[1] pos[2] pos[3]
             end
-            for s in ("velocity:", "force:   "), j in 0:num-1, k in 0:m-1
-                @printf io "Adsorbate-atom-%s %d %d     0.000000000000     0.000000000000     0.000000000000\n" s j k
-            end
             for j in 0:num-1, (k, ix) in enumerate(ffidxi)
                 @printf io "Adsorbate-atom-charge:   %d %d%19.12f\n" j (k-1) NoUnits(o.charges[ix]/u"e_au")
-            end
-            for j in 0:num-1, k in 0:m-1
-                @printf io "Adsorbate-atom-scaling:  %d %d     1.000000000000\n" j k
-            end
-            for j in 0:num-1, k in 0:m-1
-                @printf io "Adsorbate-atom-fixed:    %d %d       0 0 0\n" j k
             end
             println(io)
         end
