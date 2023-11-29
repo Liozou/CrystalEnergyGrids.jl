3e739163ba8abde95815f398c670b9cdca8bba27
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-11-29 11:46:01 +0100

    Remove length from ShootingStar

--- src/parameterinputs.jl
+++ src/parameterinputs.jl
@@ -2,14 +2,13 @@ using Base.Threads
 
 
 struct ShootingStarMinimizer{N,T}
-    length::Int
     lb::LoadBalancer{MonteCarloSetup{N,T}}
 end
-function ShootingStarMinimizer{N}(; length::Int=100) where {N}
+function ShootingStarMinimizer{N}() where {N}
     T = typeof_psystem(Val(N))
     # lb = LoadBalancer{Tuple{Int,MonteCarloSetup{N,T},SimulationSetup{RMinimumEnergy{N,T}}}}(nthreads()) do (ik, newmc, newsimu)
     lb = LoadBalancer{MonteCarloSetup{N,T}}(output_restart, 7)
-    ShootingStarMinimizer(length, lb)
+    ShootingStarMinimizer(lb)
 end
 
 function (star::ShootingStarMinimizer)(o::SimulationStep, e::Float64, k::Int, mc::MonteCarloSetup, _)
