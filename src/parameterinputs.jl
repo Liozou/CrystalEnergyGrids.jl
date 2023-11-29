a224ed2a99ba3d6abc1ed2fcce1fb04bb5b19e25
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2023-11-29 11:44:11 +0100

    Remove every

--- src/parameterinputs.jl
+++ src/parameterinputs.jl
@@ -2,21 +2,17 @@ using Base.Threads
 
 
 struct ShootingStarMinimizer{N,T}
-    every::Int
     length::Int
     lb::LoadBalancer{MonteCarloSetup{N,T}}
 end
-function ShootingStarMinimizer{N}(; length::Int=100, every::Int=1) where {N}
+function ShootingStarMinimizer{N}(; length::Int=100) where {N}
     T = typeof_psystem(Val(N))
     # lb = LoadBalancer{Tuple{Int,MonteCarloSetup{N,T},SimulationSetup{RMinimumEnergy{N,T}}}}(nthreads()) do (ik, newmc, newsimu)
     lb = LoadBalancer{MonteCarloSetup{N,T}}(output_restart, 7)
-    ShootingStarMinimizer(every, length, lb)
+    ShootingStarMinimizer(length, lb)
 end
 
 function (star::ShootingStarMinimizer)(o::SimulationStep, e::Float64, k::Int, mc::MonteCarloSetup, _)
-    k ≤ 0 && return
-    _, r = divrem(k, star.every)
-    r == 0 || return
     newmc = MonteCarloSetup(mc, o)
     put!(star.lb, newmc)
     nothing
