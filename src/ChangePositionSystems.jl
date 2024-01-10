6a8df384a4513031a4f5cfa5ce3e7e3cb2919007
Lionel Zoubritzky <lionel.zoubritzky@gmail.com>
2024-01-10 16:19:31 +0100

    Add ChangePositionSystem(s::AbstractSystem{N}; translate, rotate, bead)

--- src/ChangePositionSystems.jl
+++ src/ChangePositionSystems.jl
@@ -1,14 +1,76 @@
 # forwards calls to the underlying system, except for the positions which are fixed
 using Unitful
 
+"""
+    ChangePositionSystem{N,T<:AbstractSystem{N}} <: AbstractSystem{N}
+
+`AbstractSystem` consisting of a small layer above a reference `AbstractSystem` with
+modified positions.
+
+See [ChangePositionSystem(s::AbstractSystem{N}, poss)](@ref) and
+[ChangePositionSystem(s::AbstractSystem{N}; translate, rotate, bead)](@ref)
+to construct an instance.
+"""
 struct ChangePositionSystem{N,T<:AbstractSystem{N}} <: AbstractSystem{N}
     system::T
     positions::Vector{SVector{N,TÅ}}
 end
+
+"""
+    ChangePositionSystem(s::AbstractSystem{N}, poss) where N
+
+Construct a [`ChangePositionSystem`](@ref) consisting of the reference system `s` with
+atomic positions given by `poss`.
+
+`poss` must be an iterator over atomic coordinates (with their unit), such as a
+`Vector{SVector{N, typeof(1.0u"Å")}}` for instance.
+
+See also [ChangePositionSystem(s::AbstractSystem{N}; translate, rotate, bead)](@ref).
+"""
 ChangePositionSystem(s::AbstractSystem{N}, poss) where {N} = ChangePositionSystem{N,typeof(s)}(s, [SVector{N,TÅ}(p) for p in poss])
 ChangePositionSystem(s::ChangePositionSystem, poss) = ChangePositionSystem(s.system, poss)
 function ChangePositionSystem(s::ChangePositionSystem, poss::Vector{SVector{N,TÅ}}) where N
     ChangePositionSystem(s.system, poss) # to avoid ambiguity
+end
+
+"""
+ChangePositionSystem(s::AbstractSystem{N}; translate=nothing, rotate=nothing, bead=0) where N
+
+Construct a [`ChangePositionSystem`](@ref) consisting of the reference system `s` after
+rotation and translation.
+
+If `rotate` is set to a rotation matrix, each atom will have its position multiplied by it.
+If `bead` is set to the number of an atom of the system, the rotation will occur relative
+to that atom (i.e., that atom will not be moved).
+
+If `translate` is set to a position, each atom will have its position translated by that
+amount.
+
+If `bead == 0`, the translation (if any) occurs after the rotation (if any). Otherwise, the
+order does not matter since the rotation is relative to the position of an atom that will
+be translated with the others.
+
+See also [ChangePositionSystem(s::AbstractSystem{N}, poss)](@ref).
+"""
+function ChangePositionSystem(s::AbstractSystem{N}; translate=nothing, rotate=nothing, bead=0) where N
+    n = length(s)
+    poss = Vector{SVector{N,TÅ}}(undef, n)
+    _t = translate isa Nothing ? zero(SVector{N,TÅ}) : SVector{N,TÅ}(translate)
+    for i in 1:n
+        p = position(s, i)::SVector{N,TÅ}
+        if !(rotate isa Nothing)
+            if bead == 0
+                p = SVector{N,TÅ}(rotate * p)
+            else
+                p = p[bead] .+ SVector{N,TÅ}(rotate * (p .- p[bead]))
+            end
+        end
+        if !(translate isa Nothing)
+            p = p .+ _t
+        end
+        poss[i] = p
+    end
+    ChangePositionSystem(s, poss)
 end
 
 for f in (:boundary_conditions,
